3c31a15796bf5f0c0a2879b0da5db9f317fedeb3
Christopher Su <christophersu9@gmail.com>
2015-07-18 16:40:24 -0700

    streak checker

--- routes.py
+++ routes.py
@@ -1,4 +1,6 @@
+import arrow
 from flask import Blueprint, request, jsonify
+import requests
 
 from common import require_secret
 from config import config
@@ -8,6 +10,25 @@ module = Blueprint(config['module_name'], __name__)
 
 @module.route('/')
 @require_secret
-def get_all_checkins():
-    return jsonify(client.users.checkins())
+def get_events():
+    return jsonify({'response': requests.get('https://api.github.com/users/csu/events').json()})
+
+STREAK_EVENTS = ['CreateEvent', 'PushEvent']
+
+@module.route('/streak')
+@module.route('/streak/')
+@require_secret
+def check_streak():
+    today = arrow.now().floor('day')
+    events = requests.get('https://api.github.com/users/csu/events').json()
+    response = {'status': 'ok'}
+
+    occurred_today = []
+    for event in events:
+        if arrow.get(event["created_at"]) > today:
+            if event["type"] in STREAK_EVENTS:
+                response['result'] = True
+                return jsonify(response)
     
+    response['result'] = False
+    return jsonify(response)
